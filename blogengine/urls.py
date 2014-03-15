98ab638a3df27cecb3ec2cc21222b8c7abcd7f7a
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-03-15 14:35:43 +0700

    Implemented pagination

--- blogengine/urls.py
+++ blogengine/urls.py
@@ -4,5 +4,6 @@ from blogengine.models import Post
 
 urlpatterns = patterns('',
     # Index
-    url(r'^$', ListView.as_view(model=Post)),
+    url(r'^(?P<page>\d+)?/?$', ListView.as_view(model=Post,
+                                paginate_by=5)),
 )
